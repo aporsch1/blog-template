77e055108918df714120dc5cc13233d1ca33277b
aporsch1 <108027459+aporsch1@users.noreply.github.com>
2024-09-14 20:15:38 -0400

    customization and removal of social media garbage

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -4,16 +4,14 @@ interface SiteConfig {
 	description: string
 	lang: string
 	ogLocale: string
-	shareMessage: string
 	paginationSize: number
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
-	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
+	author: 'Abraham Porschet', // Site author
+	title: 'Unbecoming', // Site title.
+	description: 'Writing about literature, art, life', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
 	paginationSize: 6 // Number of posts per page
 }
